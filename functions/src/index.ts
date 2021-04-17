4c30b624629ace4d84fb1b7c4ce199f31f7f2c00
cloped <educarvalholopes98@gmail.com>
2021-04-17 18:52:30 -0400

    feat: express and routing on function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,32 @@
 import * as functions from "firebase-functions";
+import * as express from "express";
+import {Request, Response} from "express";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const app = express();
+
+const getExample = (req: Request, res: Response) => {
+  functions.logger.info(
+      "Get Example from login path /", {structuredData: true},
+  );
+  res.send("Get login root");
+};
+
+const postExampleAuth = (req: Request, res: Response) => {
+  functions.logger.info(
+      "Post Example from login path /auth", {structuredData: true},
+  );
+  res.send("Login auth");
+};
+
+const postExampleRegister = (req: Request, res: Response) => {
+  functions.logger.info(
+      "Post Example from login path /register", {structuredData: true}
+  );
+  res.send("Login Register");
+};
+
+app.get("/", getExample);
+app.post("/auth", postExampleAuth);
+app.post("/register", postExampleRegister);
+
+export const login = functions.https.onRequest(app);
