e63541165b0ed814b3fdec64ae035ffa588f629b
cloped <educarvalholopes98@gmail.com>
2021-04-18 22:08:27 -0400

    build: correct host on apollo deploy

--- functions/src/graphql.ts
+++ functions/src/graphql.ts
@@ -1,21 +1,32 @@
 import * as admin from "firebase-admin";
+import * as functions from "firebase-functions";
 import * as express from "express";
-import {ApolloServer} from "apollo-server-express";
+import {ApolloServer, PlaygroundConfig} from "apollo-server-express";
 
 import schema from "./schema";
 import resolvers from "./resolvers";
 
 const gqlServer = (
-    firestoreDatabase: admin.firestore.Firestore
+    firestoreDatabase: admin.firestore.Firestore,
+    configFirebase: functions.config.Config,
 ): express.Express => {
   const app = express();
+
+  const playground: boolean | PlaygroundConfig = (() => {
+    if (configFirebase?.apollo?.env === "development") {
+      return {
+        endpoint: "https://us-central1-dev-podcastie.cloudfunctions.net/apollo",
+      };
+    }
+    return true;
+  })();
 
   const apolloServer = new ApolloServer({
     typeDefs: schema,
     resolvers,
     // Enable graphiql gui
     introspection: true,
-    playground: true,
+    playground,
     context: {
       firestoreDatabase,
     },
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,9 +2,11 @@ import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 import gqlServer from "./graphql";
 
+const configFirebase = functions.config();
 admin.initializeApp();
 const db: admin.firestore.Firestore = admin.firestore();
 db.settings({ignoreUndefinedProperties: true});
-const server = gqlServer(db);
+
+const server = gqlServer(db, configFirebase);
 
 export const apollo = functions.https.onRequest(server);
